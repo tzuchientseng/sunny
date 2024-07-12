97b77dfed6dd19847dfa084a437590c28b3eaec4
Sunny Tseng <tzuchientseng@gmail.com>
2024-07-12 16:11:41 +0800

    Version 1.1

---
+++ js/password.js
@@ -0,0 +1,41 @@
+const app = Vue.createApp({
+    methods: {
+        showPasswordPrompt() {
+            Swal.fire({
+                title: 'Authentication',
+                // input: 'text',
+                input: 'password',
+                inputAttributes: {
+                    autocapitalize: 'off'
+                },
+                showCancelButton: true,
+                confirmButtonText: 'Submit',
+                confirmButtonColor: '#ff9800',  // 橘色確認按鈕
+                cancelButtonColor: '#aaa',  // 灰色取消按鈕
+                showLoaderOnConfirm: true,
+                preConfirm: (code) => {
+                    return new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            if (code === 'pass') { 
+                                resolve();
+                            } else {
+                                Swal.fire({
+                                    icon: 'error',
+                                    title: 'Invalid code!',
+                                    text: 'Try again',
+                                    confirmButtonColor: '#3085d6', // 設置為藍色
+                                    confirmButtonText: 'OK'
+                                });
+                            }
+                        }, 200)
+                    })
+                },
+                allowOutsideClick: () => !Swal.isLoading()
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = 'skill/index.html';   
+                }
+            })
+        }
+    }
+}).mount('#app');
